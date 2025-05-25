75f5f3401722bc9daf70a89e6720351fb93bd06b
Hkz0 <haikhalrapi@gmail.com>
2025-05-25 16:50:49 +0800

    postgre test

--- api.py
+++ api.py
@@ -19,7 +19,7 @@ load_dotenv()
 
 app = Flask(__name__)
 # sqli
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 app.config['SESSION_COOKIE_SAMESITE'] = 'None'
